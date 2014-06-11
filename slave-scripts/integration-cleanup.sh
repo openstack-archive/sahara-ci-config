bf919c1a0364149cc2840d8237aa2c02da976c2c
iberezovskiy <iberezovskiy@mirantis.com>
2014-06-11 14:18:12 +0400

    Modify integration-cleanup script to cleanup diskimage jobs
    Change-Id: I2ef6231cf1c04fbd25317f39079737fea81bf091

--- slave-scripts/integration-cleanup.sh
+++ slave-scripts/integration-cleanup.sh
@@ -3,31 +3,44 @@
 cd /opt/ci/jenkins-jobs/sahara-ci-config/slave-scripts
 sleep 20
 
-JOB_TYPE=$(echo $PREV_JOB | awk -F '-' '{ print $4 }')
-if [ $JOB_TYPE == 'vanilla1' ]
-then
-     JOB_TYPE=vanilla-v1
-elif [ $JOB_TYPE == 'vanilla2' ]
-then
-     JOB_TYPE=vanilla-v2
-fi
-
-if [ $JOB_TYPE == 'hdp1' ]
-then
-     JOB_TYPE=hdp
-elif [ $JOB_TYPE == 'hdp2' ]
-then
-     JOB_TYPE=hdp-v2
-fi
-
-if [ $JOB_TYPE == 'heat' ]
-then
-    JOB_TYPE=$(echo $PREV_JOB | awk -F '-' '{ print $5 }')
+JOB_TYPE=$(echo $PREV_JOB | awk -F '-' '{ print $1 }')
+if [ $JOB_TYPE == 'diskimage' ]; then
+    PLUGIN=$(echo $PREV_JOB | awk -F '-' '{ print $3 }')
+    if [ $PLUGIN == 'vanilla' ]; then
+        python cleanup.py cleanup -$PREV_BUILD-$PLUGIN-v1
+        python cleanup.py cleanup -$PREV_BUILD-$PLUGIN-v2
+    elif [ $PLUGIN == 'hdp1' ]; then
+        python cleanup.py cleanup -$PREV_BUILD-hdp
+    elif [ $PLUGIN == 'hdp2' ]; then
+        python cleanup.py cleanup -$PREV_BUILD-hdp-v2
+    else
+        python cleanup.py cleanup -$PREV_BUILD-$PLUGIN
+    fi
+else
+    JOB_TYPE=$(echo $PREV_JOB | awk -F '-' '{ print $4 }')
     if [ $JOB_TYPE == 'vanilla1' ]
     then
         JOB_TYPE=vanilla-v1
+    elif [ $JOB_TYPE == 'vanilla2' ]
+    then
+        JOB_TYPE=vanilla-v2
     fi
-    python cleanup.py cleanup-heat ci-$PREV_BUILD-$JOB_TYPE
-else                                                                            
-    python cleanup.py cleanup -$PREV_BUILD-$JOB_TYPE
+    if [ $JOB_TYPE == 'hdp1' ]
+    then
+        JOB_TYPE=hdp
+    elif [ $JOB_TYPE == 'hdp2' ]
+    then
+        JOB_TYPE=hdp-v2
+    fi
+    if [ $JOB_TYPE == 'heat' ]
+    then
+       JOB_TYPE=$(echo $PREV_JOB | awk -F '-' '{ print $5 }')
+        if [ $JOB_TYPE == 'vanilla1' ]
+        then
+            JOB_TYPE=vanilla-v1
+        fi
+        python cleanup.py cleanup-heat ci-$PREV_BUILD-$JOB_TYPE
+    else                                                                            
+        python cleanup.py cleanup -$PREV_BUILD-$JOB_TYPE
+    fi
 fi
