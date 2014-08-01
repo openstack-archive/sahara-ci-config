686a27b6ace21d4318e94dc54ed080c6b6d63302
Denis Egorenko <degorenko@mirantis.com>
2014-08-01 13:27:32 +0400

    Refactoring of CI scripts
    Change-Id: I78834f9f9e7e023b2684463897ad05bb77c1122a

--- slave-scripts/diskimage-creating-integration.sh
+++ slave-scripts/diskimage-creating-integration.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-NETWORK=`ifconfig eth0 | awk -F ' *|:' '/inet addr/{print $4}' | awk -F . '{print $2}'`
-if [ "$NETWORK" == "0" ]; then
-    OPENSTACK_HOST="172.18.168.42"
-    HOST="c1"
-    TENANT_ID="$CI_LAB_TENANT_ID"
-else
-    OPENSTACK_HOST="172.18.168.43"
-    HOST="c2"
-    TENANT_ID="$STACK_SAHARA_TENANT_ID"
-fi
+. ./common-scripts.sh
+
+check_openstack_host
 
 check_error_code() {
    if [ "$1" != "0" -o ! -f "$2" ]; then
@@ -87,8 +80,7 @@ rename_image() {
 
 plugin="$1"
 image_type=${2:-ubuntu}
-hadoop_version=${3:-1}
-TIMEOUT=60
+HADOOP_VERSION=${3:-1}
 GERRIT_CHANGE_NUMBER=$ZUUL_CHANGE
 SKIP_CINDER_TEST=True
 SKIP_CLUSTER_CONFIG_TEST=True
@@ -97,6 +89,7 @@ SKIP_MAP_REDUCE_TEST=False
 SKIP_SWIFT_TEST=True
 SKIP_SCALING_TEST=True
 SKIP_TRANSIENT_TEST=True
+ONLY_TRANSIENT_TEST=False
 VANILLA_IMAGE=$HOST-sahara-vanilla-${image_type}-${GERRIT_CHANGE_NUMBER}-hadoop_1
 VANILLA_TWO_IMAGE=$HOST-sahara-vanilla-${image_type}-${GERRIT_CHANGE_NUMBER}-hadoop_2
 HDP_IMAGE=$HOST-sahara-hdp-centos-${GERRIT_CHANGE_NUMBER}-hadoop_1
@@ -118,23 +111,26 @@ case $plugin in
        fi
        SSH_USERNAME=${username}
 
-       case $hadoop_version in
+       case $HADOOP_VERSION in
            1)
               sudo ${image_type}_vanilla_hadoop_1_image_name=${VANILLA_IMAGE} JAVA_DOWNLOAD_URL='http://127.0.0.1:8000/jdk-7u51-linux-x64.tar.gz' SIM_REPO_PATH=$WORKSPACE bash diskimage-create/diskimage-create.sh -p vanilla -i $image_type -v 1
               check_error_code $? ${VANILLA_IMAGE}.qcow2
               upload_image "vanilla-1" "${username}" ${VANILLA_IMAGE}
+              PLUGIN_TYPE=vanilla1
               ;;
            2.3)
               sudo ${image_type}_vanilla_hadoop_2_3_image_name=${VANILLA_TWO_IMAGE} JAVA_DOWNLOAD_URL='http://127.0.0.1:8000/jdk-7u51-linux-x64.tar.gz' SIM_REPO_PATH=$WORKSPACE bash diskimage-create/diskimage-create.sh -p vanilla -i $image_type -v 2.3
               check_error_code $? ${VANILLA_TWO_IMAGE}.qcow2
               upload_image "vanilla-2.3" "${username}" ${VANILLA_TWO_IMAGE}
-              hadoop_version=2-3
+              HADOOP_VERSION=2-3
+              PLUGIN_TYPE=vanilla2
               ;;
            2.4)
               sudo ${image_type}_vanilla_hadoop_2_4_image_name=${VANILLA_TWO_IMAGE} JAVA_DOWNLOAD_URL='http://127.0.0.1:8000/jdk-7u51-linux-x64.tar.gz' SIM_REPO_PATH=$WORKSPACE bash diskimage-create/diskimage-create.sh -p vanilla -i $image_type -v 2.4
               check_error_code $? ${VANILLA_TWO_IMAGE}.qcow2
               upload_image "vanilla-2.4" "${username}" ${VANILLA_TWO_IMAGE}
-              hadoop_version=2-4
+              HADOOP_VERSION=2-4
+              PLUGIN_TYPE=vanilla2
               ;;
        esac
     ;;
@@ -147,6 +143,7 @@ case $plugin in
        image_type="ubuntu"
        sudo ${image_type}_spark_image_name=${SPARK_IMAGE} JAVA_DOWNLOAD_URL='http://127.0.0.1:8000/jdk-7u51-linux-x64.tar.gz' SIM_REPO_PATH=$WORKSPACE bash diskimage-create/diskimage-create.sh -p "spark"
        check_error_code $? ${SPARK_IMAGE}.qcow2
+       PLUGIN_TYPE=$plugin
        exit 0
     ;;
 
@@ -156,6 +153,7 @@ case $plugin in
        check_error_code $? ${HDP_IMAGE}.qcow2
        SSH_USERNAME="root"
        upload_image "hdp1" "root" ${HDP_IMAGE}
+       PLUGIN_TYPE=$plugin
     ;;
 
     hdp2)
@@ -164,7 +162,8 @@ case $plugin in
        check_error_code $? ${HDP_TWO_IMAGE}.qcow2
        SSH_USERNAME="root"
        upload_image "hdp2" "root" ${HDP_TWO_IMAGE}
-       hadoop_version="2"
+       HADOOP_VERSION="2"
+       PLUGIN_TYPE=$plugin
     ;;
 
     cdh)
@@ -173,7 +172,8 @@ case $plugin in
        check_error_code $? ${CDH_IMAGE}.qcow2
        upload_image "cdh" "ubuntu" ${CDH_IMAGE}
        SSH_USERNAME="ubuntu"
-       hadoop_version="2"
+       HADOOP_VERSION="2"
+       PLUGIN_TYPE=$plugin
     ;;
 esac
 
@@ -186,235 +186,29 @@ if [ "$image_type" == "fedora" ]; then
     image_os="fOS"
 fi
 
-# Run test
-export PYTHONUNBUFFERED=1
-
 cd /tmp/
-
 TOX_LOG=/tmp/sahara/.tox/venv/log/venv-1.log
-TMP_LOG=/tmp/tox.log
-LOG_FILE=/tmp/tox_log.log
 
-SCR_CHECK=$(ps aux | grep screen | grep sahara)
-if [ -n "$SCR_CHECK" ]; then
-     screen -S sahara-all -X quit
-fi
-
-rm -rf /tmp/cache
-rm -f $LOG_FILE
-
-mysql -usahara-citest -psahara-citest -Bse "DROP DATABASE IF EXISTS sahara"
-mysql -usahara-citest -psahara-citest -Bse "create database sahara"
-
-BUILD_ID=dontKill
-#sudo pip install tox
-mkdir /tmp/cache
-
-export ADDR=`ifconfig eth0| awk -F ' *|:' '/inet addr/{print $4}'`
+create_database
 
 sudo rm -rf sahara
 git clone https://review.openstack.org/openstack/sahara
 cd sahara
 sudo pip install .
 
-echo "[DEFAULT]
-" >> etc/sahara/sahara.conf
+enable_pypi
 
-echo "infrastructure_engine=direct
-" >> etc/sahara/sahara.conf
-
-if [ "$plugin" == "cdh" ]
-then
-    echo "plugins=vanilla,hdp,cdh
-" >> etc/sahara/sahara.conf
-fi
-
-echo "
-os_auth_host=$OPENSTACK_HOST
-os_auth_port=5000
-os_admin_username=ci-user
-os_admin_password=nova
-os_admin_tenant_name=ci
-use_identity_api_v3=true
-use_neutron=true
-node_domain = nl
-[database]
-connection=mysql://sahara-citest:sahara-citest@localhost/sahara?charset=utf8
-[keystone_authtoken]
-auth_uri=http://$OPENSTACK_HOST:5000/v2.0/
-identity_uri=http://$OPENSTACK_HOST:35357/
-admin_user=ci-user
-admin_password=nova
-admin_tenant_name=ci" >> etc/sahara/sahara.conf
-
-echo "----------- sahara.conf -----------"
-cat etc/sahara/sahara.conf
-echo "----------- end of sahara.conf -----------"
-
-#touch ~/.pip/pip.conf
-
-#echo "
-#[global]
-#timeout = 60
-#index-url = http://sahara-ci.vm.mirantis.net/pypi/sahara/
-#extra-index-url = https://pypi.python.org/simple/
-#download-cache = /home/ubuntu/.pip/cache/
-#[install]
-#use-mirrors = true
-#find-links = http://sahara-ci.vm.mirantis.net:8181/simple/
-#" > ~/.pip/pip.conf
-echo "
-[global]
-timeout = 60
-index-url = https://sahara.mirantis.com/pypi/
-extra-index-url = http://pypi.openstack.org/openstack/
-download-cache = /home/jenkins/.pip/cache/
-[install]
-use-mirrors = true
-" > ~/.pip/pip.conf
-
-echo "
-[easy_install]
-index_url = https://sahara.mirantis.com/pypi/
-" > ~/.pydistutils.cfg
-
-sahara-db-manage --config-file etc/sahara/sahara.conf upgrade head
-
-screen -dmS sahara-all /bin/bash -c "PYTHONUNBUFFERED=1 sahara-all --config-file etc/sahara/sahara.conf -d --log-file log.txt | tee /tmp/tox-log.txt"
+write_sahara_main_conf etc/sahara/sahara.conf
+start_sahara etc/sahara/sahara.conf
 
 cd /tmp/sahara
-export ADDR=`ifconfig eth0| awk -F ' *|:' '/inet addr/{print $4}'`
 
-echo "[COMMON]
-OS_USERNAME = 'ci-user'
-OS_PASSWORD = 'nova'
-OS_TENANT_NAME = 'ci'
-OS_TENANT_ID = '$TENANT_ID'
-OS_AUTH_URL = 'http://$OPENSTACK_HOST:5000/v2.0'
-SAHARA_HOST = '$ADDR'
-FLAVOR_ID = '20'
-CLUSTER_CREATION_TIMEOUT = $TIMEOUT
-CLUSTER_NAME = '$HOST-$image_os-$hadoop_version-$BUILD_NUMBER-$ZUUL_CHANGE-$ZUUL_PATCHSET'
-FLOATING_IP_POOL = 'public'
-NEUTRON_ENABLED = True
-INTERNAL_NEUTRON_NETWORK = 'private'
-JOB_LAUNCH_TIMEOUT = 15
-HDFS_INITIALIZATION_TIMEOUT = 10
-$COMMON_PARAMS
-" >> sahara/tests/integration/configs/itest.conf
+CLUSTER_NAME="$HOST-$image_os-$HADOOP_VERSION-$BUILD_NUMBER-$ZUUL_CHANGE-$ZUUL_PATCHSET"
+write_tests_conf sahara/tests/integration/configs/itest.conf
 
-echo "[VANILLA]
-SSH_USERNAME = '$SSH_USERNAME'
-IMAGE_NAME = '$VANILLA_IMAGE'
-SKIP_CINDER_TEST = '$SKIP_CINDER_TEST'
-SKIP_CLUSTER_CONFIG_TEST = $SKIP_CLUSTER_CONFIG_TEST
-SKIP_EDP_TEST = $SKIP_EDP_TEST
-SKIP_MAP_REDUCE_TEST = $SKIP_MAP_REDUCE_TEST
-SKIP_SWIFT_TEST = $SKIP_SWIFT_TEST
-SKIP_SCALING_TEST = $SKIP_SCALING_TEST
-SKIP_TRANSIENT_CLUSTER_TEST = $SKIP_TRANSIENT_TEST
-$VANILLA_PARAMS
-" >> sahara/tests/integration/configs/itest.conf
+run_tests
 
-echo "[VANILLA_TWO]
-SSH_USERNAME = '$SSH_USERNAME'
-IMAGE_NAME = '$VANILLA_TWO_IMAGE'
-SKIP_CINDER_TEST = '$SKIP_CINDER_TEST'
-SKIP_MAP_REDUCE_TEST = $SKIP_MAP_REDUCE_TEST
-SKIP_SWIFT_TEST = $SKIP_SWIFT_TEST
-SKIP_SCALING_TEST = $SKIP_SCALING_TEST
-$VANILLA_PARAMS
-" >> sahara/tests/integration/configs/itest.conf
-
-if [ "$plugin" == "vanilla" -a "$hadoop_version" == "2-4" ]; then
-   echo "HADOOP_VERSION = '2.4.1'
-SKIP_EDP_JOB_TYPES = Pig
-HADOOP_EXAMPLES_JAR_PATH = '/opt/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.1.jar'
-" >> sahara/tests/integration/configs/itest.conf
-fi
-
-echo "[HDP]
-SSH_USERNAME = '$SSH_USERNAME'
-IMAGE_NAME = '$HDP_IMAGE'
-SKIP_ALL_TESTS_FOR_PLUGIN = False
-SKIP_CINDER_TEST = '$SKIP_CINDER_TEST'
-SKIP_EDP_TEST = $SKIP_EDP_TEST
-SKIP_MAP_REDUCE_TEST = $SKIP_MAP_REDUCE_TEST
-SKIP_SWIFT_TEST = $SKIP_SWIFT_TEST
-SKIP_SCALING_TEST = $SKIP_SCALING_TEST
-$HDP_PARAMS
-" >> sahara/tests/integration/configs/itest.conf
-
-echo "[HDP2]
-SSH_USERNAME = '$SSH_USERNAME'
-IMAGE_NAME = '$HDP_TWO_IMAGE'
-SKIP_ALL_TESTS_FOR_PLUGIN = False
-SKIP_SCALING_TEST = $SKIP_SCALING_TEST
-" >> sahara/tests/integration/configs/itest.conf
-
-echo "[CDH]
-SSH_USERNAME = '$SSH_USERNAME'
-IMAGE_NAME = '$CDH_IMAGE'
-" >> $WORKSPACE/sahara/tests/integration/configs/itest.conf
-
-touch $TMP_LOG
-API_RESPONDING_TIMEOUT=30
-FAILURE=0
-
-if ! timeout ${API_RESPONDING_TIMEOUT} sh -c "while ! curl -s http://127.0.0.1:8386/v1.1/ 2>/dev/null | grep -q 'Authentication required' ; do sleep 1; done"; then
-    echo "Sahara API failed to respond within ${API_RESPONDING_TIMEOUT} seconds"
-    FAILURE=1
-fi
-
-if [ "$FAILURE" = 0 ]; then
-
-    export PYTHONUNBUFFERED=1
-
-    cd /tmp/sahara
-    if [ "${plugin}" == "vanilla" ]; then
-        if [ "${hadoop_version}" == "1" ]; then
-           tox -e integration -- vanilla1 --concurrency=1
-           STATUS=`echo $?`
-        else
-           tox -e integration -- vanilla2 --concurrency=1
-           STATUS=`echo $?`
-        fi
-    fi
-    if [ "${plugin}" == "hdp1" ]; then
-        tox -e integration -- hdp1 --concurrency=1
-        STATUS=`echo $?`
-    fi
-    if [ "${plugin}" == "hdp2" ]; then
-        tox -e integration -- hdp2 --concurrency=1
-        STATUS=`echo $?`
-    fi
-    if [ "${plugin}" == "cdh" ]
-    then
-        tox -e integration -- cdh --concurrency=1
-        STATUS=`echo $?`
-    fi
-fi
-
-echo "-----------Python integration env-----------"
-cd /tmp/sahara && .tox/integration/bin/pip freeze
-
-screen -S sahara-all -X quit
-
-echo "-----------Python sahara env-----------"
-cd /tmp/sahara && pip freeze
-
-echo "-----------Sahara Log------------"
-cat /tmp/sahara/log.txt
-rm -rf /tmp/sahara
-rm -rf /tmp/cache/
-
-echo "-----------Tox log-----------"
-cat /tmp/tox-log.txt
-rm -f /tmp/tox-log.txt
-
-rm $TMP_LOG
-rm -f $LOG_FILE
-cd $HOME
+cat_logs /tmp/sahara
 
 if [ "$FAILURE" != 0 ]; then
     exit 1
@@ -423,7 +217,7 @@ fi
 if [[ "$STATUS" != 0 ]]
 then
     if [ "${plugin}" == "vanilla" ]; then
-        if [ "${hadoop_version}" == "1" ]; then
+        if [ "${HADOOP_VERSION}" == "1" ]; then
             delete_image $VANILLA_IMAGE
         else
             delete_image $VANILLA_TWO_IMAGE
@@ -444,7 +238,7 @@ fi
 if [ "$ZUUL_PIPELINE" == "check" ]
 then
     if [ "${plugin}" == "vanilla" ]; then
-        if [ "${hadoop_version}" == "1" ]; then
+        if [ "${HADOOP_VERSION}" == "1" ]; then
             delete_image $VANILLA_IMAGE
         else
             delete_image $VANILLA_TWO_IMAGE
@@ -461,12 +255,12 @@ then
     fi
 else
     if [ "${plugin}" == "vanilla" ]; then
-        if [ "${hadoop_version}" == "1" ]; then
+        if [ "${HADOOP_VERSION}" == "1" ]; then
             delete_image ${image_type}_sahara_vanilla_hadoop_1_latest
             rename_image $VANILLA_IMAGE ${image_type}_sahara_vanilla_hadoop_1_latest
         else
-            delete_image ${image_type}_sahara_vanilla_hadoop_${hadoop_version}_latest
-            rename_image $VANILLA_TWO_IMAGE ${image_type}_sahara_vanilla_hadoop_${hadoop_version}_latest
+            delete_image ${image_type}_sahara_vanilla_hadoop_${HADOOP_VERSION}_latest
+            rename_image $VANILLA_TWO_IMAGE ${image_type}_sahara_vanilla_hadoop_${HADOOP_VERSION}_latest
         fi
     fi
     if [ "${plugin}" == "hdp1" ]; then
--- slave-scripts/gate-sahara-integration.sh
+++ slave-scripts/gate-sahara-integration.sh
@@ -3,31 +3,14 @@
 #this is to fix bug with testtools==0.9.35
 #sed 's/testtools>=0.9.32/testtools==0.9.34/' -i test-requirements.txt
 
-NETWORK=`ifconfig eth0 | awk -F ' *|:' '/inet addr/{print $4}' | awk -F . '{print $2}'`
-if [ "$NETWORK" == "0" ]; then
-    OPENSTACK_HOST="172.18.168.42"
-    HOST="c1"
-    TENANT_ID="$CI_LAB_TENANT_ID"
-else
-    OPENSTACK_HOST="172.18.168.43"
-    HOST="c2"
-    TENANT_ID="$STACK_SAHARA_TENANT_ID"
-fi
+. ./common-scripts.sh
+
+check_openstack_host
 
 sudo pip install .
 
 WORKSPACE=${1:-$WORKSPACE}
-
-export PIP_USE_MIRRORS=True
-
 JOB_TYPE=$(echo $JOB_NAME | awk -F '-' '{ print $4 }')
-TIMEOUT=60
-if [ "$ZUUL_BRANCH" == "stable/icehouse" ]
-then
-   SAHARA_BIN=sahara-api
-else
-   SAHARA_BIN=sahara-all
-fi
 
 HADOOP_VERSION=1
 SKIP_CINDER_TEST=False
@@ -38,8 +21,8 @@ SKIP_SWIFT_TEST=False
 SKIP_SCALING_TEST=False
 SKIP_TRANSIENT_TEST=True
 SKIP_ONLY_TRANSIENT_TEST=False
-HDP1_IMAGE=sahara-itests-ci-hdp-image-jdk-iptables-off
-HDP2_IMAGE=centos-6_4-64-hdp-2-0-hw
+HDP_IMAGE=sahara-itests-ci-hdp-image-jdk-iptables-off
+HDP_TWO_IMAGE=centos-6_4-64-hdp-2-0-hw
 VANILLA_IMAGE=sahara-itests-ci-vanilla-image
 CDH_IMAGE=ubuntu_cdh_latest
 HEAT_JOB=False
@@ -52,30 +35,27 @@ then
     JOB_TYPE=$(echo $JOB_TYPE | awk -F '_' '{ print $2 }')
     SKIP_TRANSIENT_TEST=True
 fi
-
 if [ $JOB_TYPE == 'hdp1' ]
 then
-   HDP1_JOB=True
    echo "HDP1 detected"
+   PLUGIN_TYPE=hdp1
+   SSH_USERNAME=root
 fi
-
 if [ $JOB_TYPE == 'hdp2' ]
 then
-   HDP2_JOB=True
    SSH_USERNAME=root
    HADOOP_VERSION=2
+   PLUGIN_TYPE=hdp2
    echo "HDP2 detected"
 fi
-
 if [ $JOB_TYPE == 'vanilla' ]
 then
    HADOOP_VERSION=$(echo $JOB_NAME | awk -F '-' '{ print $5}')
    if [ "$HADOOP_VERSION" == "1" ]; then
-       VANILLA_JOB=True
-       VANILLA_IMAGE=sahara-itests-ci-vanilla-image
+       PLUGIN_TYPE=vanilla1
        echo "Vanilla detected"
    else
-       VANILLA2_JOB=True
+       PLUGIN_TYPE=vanilla2
        if [ "$HADOOP_VERSION" == "2.3" ]; then
           VANILLA_TWO_IMAGE=ubuntu-vanilla-2.3-latest
           HADOOP_VERSION=2-3
@@ -88,6 +68,7 @@ then
 fi
 if [ $JOB_TYPE == 'transient' ]
 then
+   PLUGIN_TYPE=transient
    SKIP_EDP_TEST=False
    SKIP_TRANSIENT_TEST=False
    SKIP_ONLY_TRANSIENT_TEST=True
@@ -96,258 +77,29 @@ then
 fi
 if [ $JOB_TYPE == 'cdh' ]
 then
-   CDH_JOB=True
+   PLUGIN_TYPE=cdh
    SSH_USERNAME=ubuntu
    echo "CDH detected"
 fi
 
-export PYTHONUNBUFFERED=1
-
 cd $WORKSPACE
 
 TOX_LOG=$WORKSPACE/.tox/venv/log/venv-1.log
-TMP_LOG=/tmp/tox.log
-LOG_FILE=/tmp/tox_log.log
 
-SCR_CHECK=$(ps aux | grep screen | grep sahara)
-if [ -n "$SCR_CHECK" ]; then
-     screen -S sahara-all -X quit
-fi
+create_database
+#enable_pypi
 
-rm -rf /tmp/cache
-rm -f $LOG_FILE
+write_sahara_main_conf etc/sahara/sahara.conf
+start_sahara etc/sahara/sahara.conf
 
-mysql -usahara-citest -psahara-citest -Bse "DROP DATABASE IF EXISTS sahara"
-mysql -usahara-citest -psahara-citest -Bse "create database sahara"
+cd $WORKSPACE
 
-BUILD_ID=dontKill
+CLUSTER_NAME="$HOST-$HADOOP_VERSION-$BUILD_NUMBER-$ZUUL_CHANGE-$ZUUL_PATCHSET"
+write_tests_conf $WORKSPACE/sahara/tests/integration/configs/itest.conf
 
-#sudo pip install tox
-mkdir /tmp/cache
+run_tests
 
-export ADDR=`ifconfig eth0| awk -F ' *|:' '/inet addr/{print $4}'`
-
-echo "[DEFAULT]
-" >> etc/sahara/sahara.conf
-
-if [ "$HEAT_JOB" = True ]
-then
-    echo "infrastructure_engine=heat
-    " >> etc/sahara/sahara.conf
-else
-    echo "infrastructure_engine=direct
-    " >> etc/sahara/sahara.conf
-fi
-
-if [ "$CDH_JOB" ]
-then
-    echo "plugins=vanilla,hdp,cdh
-    " >> etc/sahara/sahara.conf
-fi
-
-echo "
-os_auth_host=$OPENSTACK_HOST
-os_auth_port=5000
-os_admin_username=ci-user
-os_admin_password=nova
-os_admin_tenant_name=ci
-use_identity_api_v3=true
-use_neutron=true
-min_transient_cluster_active_time=30
-node_domain=nl
-[database]
-connection=mysql://sahara-citest:sahara-citest@localhost/sahara?charset=utf8
-[keystone_authtoken]
-auth_uri=http://$OPENSTACK_HOST:5000/v2.0/
-identity_uri=http://$OPENSTACK_HOST:35357/
-admin_user=ci-user
-admin_password=nova
-admin_tenant_name=ci
-" >> etc/sahara/sahara.conf
-
-echo "----------- sahara.conf -----------"
-cat etc/sahara/sahara.conf
-echo "----------- end of sahara.conf -----------"
-
-#touch ~/.pip/pip.conf
-
-#echo "
-#[global]
-#timeout = 60
-#index-url = https://sahara.mirantis.com/pypi/
-#extra-index-url = http://pypi.openstack.org/openstack/
-#download-cache = /home/jenkins/.pip/cache/
-#[install]
-#use-mirrors = true
-#" > ~/.pip/pip.conf
-
-#echo "
-#[easy_install]
-#index_url = https://sahara.mirantis.com/pypi/
-#" > ~/.pydistutils.cfg
-
-sahara-db-manage --config-file etc/sahara/sahara.conf upgrade head
-STATUS=`echo $?`
-if [[ "$STATUS" != 0 ]]
-then
-    exit 1
-fi
-
-screen -dmS sahara-all /bin/bash -c "PYTHONUNBUFFERED=1 $SAHARA_BIN --config-file etc/sahara/sahara.conf -d --log-file log.txt | tee /tmp/tox-log.txt"
-
-
-export ADDR=`ifconfig eth0| awk -F ' *|:' '/inet addr/{print $4}'`
-
-echo "[COMMON]
-OS_USERNAME = 'ci-user'
-OS_PASSWORD = 'nova'
-OS_TENANT_NAME = 'ci'
-OS_TENANT_ID = '$TENANT_ID'
-OS_AUTH_URL = 'http://$OPENSTACK_HOST:5000/v2.0'
-SAHARA_HOST = '$ADDR'
-FLAVOR_ID = '20'
-CLUSTER_CREATION_TIMEOUT = $TIMEOUT
-CLUSTER_NAME = '$HOST-$HADOOP_VERSION-$BUILD_NUMBER-$ZUUL_CHANGE-$ZUUL_PATCHSET'
-FLOATING_IP_POOL = 'public'
-NEUTRON_ENABLED = True
-INTERNAL_NEUTRON_NETWORK = 'private'
-JOB_LAUNCH_TIMEOUT = 15
-HDFS_INITIALIZATION_TIMEOUT = 10
-$COMMON_PARAMS
-" >> $WORKSPACE/sahara/tests/integration/configs/itest.conf
-
-echo "[VANILLA]
-SSH_USERNAME = '$SSH_USERNAME'
-IMAGE_NAME = '$VANILLA_IMAGE'
-SKIP_CINDER_TEST = '$SKIP_CINDER_TEST'
-SKIP_CLUSTER_CONFIG_TEST = $SKIP_CLUSTER_CONFIG_TEST
-SKIP_EDP_TEST = $SKIP_EDP_TEST
-SKIP_MAP_REDUCE_TEST = $SKIP_MAP_REDUCE_TEST
-SKIP_SWIFT_TEST = $SKIP_SWIFT_TEST
-SKIP_SCALING_TEST = $SKIP_SCALING_TEST
-SKIP_TRANSIENT_CLUSTER_TEST = $SKIP_TRANSIENT_TEST
-ONLY_TRANSIENT_CLUSTER_TEST = $SKIP_ONLY_TRANSIENT_TEST
-$VANILLA_PARAMS
-" >> $WORKSPACE/sahara/tests/integration/configs/itest.conf
-
-echo "[VANILLA_TWO]
-SSH_USERNAME = '$SSH_USERNAME'
-IMAGE_NAME = '$VANILLA_TWO_IMAGE'
-SKIP_CINDER_TEST = '$SKIP_CINDER_TEST'
-SKIP_MAP_REDUCE_TEST = $SKIP_MAP_REDUCE_TEST
-SKIP_SWIFT_TEST = $SKIP_SWIFT_TEST
-SKIP_SCALING_TEST = $SKIP_SCALING_TEST
-$VANILLA_PARAMS
-" >> $WORKSPACE/sahara/tests/integration/configs/itest.conf
-
-if [ "$JOB_TYPE" == "vanilla" -a "$HADOOP_VERSION" == "2-4" ]; then
-   echo "HADOOP_VERSION = '2.4.1'
-HADOOP_EXAMPLES_JAR_PATH = '/opt/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.1.jar'
-SKIP_EDP_JOB_TYPES = Pig
-" >> sahara/tests/integration/configs/itest.conf
-fi
-
-echo "[HDP]
-SSH_USERNAME = '$SSH_USERNAME'
-IMAGE_NAME = '$HDP1_IMAGE'
-SKIP_ALL_TESTS_FOR_PLUGIN = False
-SKIP_CINDER_TEST = '$SKIP_CINDER_TEST'
-SKIP_EDP_TEST = $SKIP_EDP_TEST
-SKIP_MAP_REDUCE_TEST = $SKIP_MAP_REDUCE_TEST
-SKIP_SWIFT_TEST = $SKIP_SWIFT_TEST
-SKIP_SCALING_TEST = $SKIP_SCALING_TEST
-$HDP1_PARAMS
-" >> $WORKSPACE/sahara/tests/integration/configs/itest.conf
-
-echo "[HDP2]
-SSH_USERNAME = '$SSH_USERNAME'
-IMAGE_NAME = '$HDP2_IMAGE'
-SKIP_ALL_TESTS_FOR_PLUGIN = False
-" >> $WORKSPACE/sahara/tests/integration/configs/itest.conf
-
-echo "[CDH]
-SSH_USERNAME = '$SSH_USERNAME'
-IMAGE_NAME = '$CDH_IMAGE'
-" >> $WORKSPACE/sahara/tests/integration/configs/itest.conf
-
-touch $TMP_LOG
-API_RESPONDING_TIMEOUT=30
-FAILURE=0
-
-if ! timeout ${API_RESPONDING_TIMEOUT} sh -c "while ! curl -s http://127.0.0.1:8386/v1.1/ 2>/dev/null | grep -q 'Authentication required' ; do sleep 1; done"; then
-    echo "Sahara API failed to respond within ${API_RESPONDING_TIMEOUT} seconds"
-    FAILURE=1
-fi
-
-if [ "$FAILURE" = 0 ]; then
-
-    echo "project is started"
-    export PYTHONUNBUFFERED=1
-
-    cd $WORKSPACE
-    if [ $HDP1_JOB ]
-    then
-        if [ "$ZUUL_BRANCH" == "stable/icehouse" ]
-        then
-            tox -e integration -- hdp --concurrency=1
-            STATUS=`echo $?`
-        else
-            tox -e integration -- hdp1 --concurrency=1
-            STATUS=`echo $?`
-        fi
-    fi
-
-    if [ $HDP2_JOB ]
-    then
-        tox -e integration -- hdp2 --concurrency=1
-        STATUS=`echo $?`
-    fi
-
-    if [ $VANILLA_JOB ]
-    then
-        tox -e integration -- vanilla1 --concurrency=1
-        STATUS=`echo $?`
-    fi
-
-    if [ $VANILLA2_JOB ]
-    then
-        tox -e integration -- vanilla2 --concurrency=1
-        STATUS=`echo $?`
-    fi
-
-    if [ $TRANSIENT_JOB ]
-    then
-        tox -e integration -- transient --concurrency=1
-        STATUS=`echo $?`
-    fi
-
-    if [ $CDH_JOB ]
-    then
-        tox -e integration -- cdh --concurrency=1
-        STATUS=`echo $?`
-    fi
-
-fi
-
-echo "-----------Python integration env-----------"
-cd $WORKSPACE && .tox/integration/bin/pip freeze
-
-screen -S sahara-all -X quit
-
-echo "-----------Python sahara env-----------"
-pip freeze
-
-echo "-----------Sahara Log------------"
-cat $WORKSPACE/log.txt
-rm -rf /tmp/workspace/
-rm -rf /tmp/cache/
-
-echo "-----------Tox log-----------"
-cat /tmp/tox-log.txt
-rm -f /tmp/tox-log.txt
-
-rm $TMP_LOG
-rm -f $LOG_FILE
+cat_logs $WORKSPACE
 
 if [ "$FAILURE" != 0 ]; then
     exit 1
