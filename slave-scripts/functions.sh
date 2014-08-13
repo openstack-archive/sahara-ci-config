cb4d64dd11d7b1a9ca21d53f810b4d8bdd7c5b1a
Denis Egorenko <degorenko@mirantis.com>
2014-08-13 12:45:41 +0400

    Fixing bugs after refactoring
    * etting variable 'HADOOP_VERSION' to lowercase
    * adding missed run 'check_openstack_host'
    * adding missed word 'SKIP_' to the name of variable
    'ONLY_TRANSIENT_TEST'
    
    Change-Id: Icb2d70232388e6947a0059dcc499c704569491e5

--- slave-scripts/functions.sh
+++ slave-scripts/functions.sh
@@ -149,8 +149,8 @@ SKIP_SWIFT_TEST = $SKIP_SWIFT_TEST
 SKIP_SCALING_TEST = $SKIP_SCALING_TEST
 " >> $test_conf_path
 
-if [ $PLUGIN_TYPE == "vanilla2" -a "$HADOOP_VERSION" == "2-4" ]; then
-     echo "HADOOP_VERSION = '2.4.1'
+if [ $PLUGIN_TYPE == "vanilla2" -a "$hadoop_version" == "2-4" ]; then
+     echo "hadoop_version = '2.4.1'
 SKIP_EDP_JOB_TYPES = Pig
 HADOOP_EXAMPLES_JAR_PATH = '/opt/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.1.jar'
 " >> $test_conf_path
