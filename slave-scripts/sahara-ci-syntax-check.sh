78ad30d5db17939f35649cbe823b7a4a6f4e310a
Denis Egorenko <degorenko@mirantis.com>
2014-11-27 13:59:37 +0400

    Add syntax check job for CI scripts
    Change-Id: I7cf6f53f3e13caaf1ff7c4ef26b9f41ad478809e

---
+++ slave-scripts/sahara-ci-syntax-check.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+
+FAILED_TESTS=""
+
+check_return_code() {
+  code=$1
+  test_name=$2
+  if [ $code -ne 0 ]; then
+    FAILED_TESTS+="${test_name} syntax wrong; "
+  fi
+}
+
+echo "Check bash syntax..."
+bash -n `find . -name "*.sh"`
+check_return_code $? "Bash"
+
+echo "Check python syntax..."
+python -m py_compile `find . -name "*.py"`
+check_return_code $? "Python"
+
+echo "Check zuul config syntax..."
+zuul-server -c config/zuul/zuul.conf -l config/zuul/layout.yaml -t
+check_return_code $? "Zuul conf"
+
+echo "Check nodepool yaml syntax..."
+ruby -e "require 'yaml'; y=YAML.load_file('config/nodepool/sahara.yaml')"
+check_return_code $? "Nodepool conf"
+
+echo "Check Jenkins Job Builder syntax..."
+jenkins-jobs test -r -o $WORKSPACE ./jenkins_job_builder
+check_return_code $? "Jenkins Job Builder"
+
+if [ -n "$FAILED_TESTS" ]; then
+   echo "$FAILED_TESTS"
+   exit 1
+else
+   echo "Syntax checks passed"
+   exit 0
+fi
