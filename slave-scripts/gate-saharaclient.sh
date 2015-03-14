ede1ad2c0c592dbe95922d28335ebbb85898a7a4
Sergey Kolekonov <skolekonov@mirantis.com>
2015-03-14 22:40:53 +0300

    Fix saharaclient jobs which use scenario tests
    Currently these jobs are broken due to incorrect venv name and config file path
    
    Change-Id: Ic930f1d79ac174cf2b351a7e660091f1898bee7e

--- slave-scripts/gate-saharaclient.sh
+++ slave-scripts/gate-saharaclient.sh
@@ -2,11 +2,22 @@
 
 sudo rm -rf /tmp/sahara
 git clone https://git.openstack.org/openstack/sahara /tmp/sahara
+CONFIG_PATH=sahara-ci-config/config/sahara
+
+JOB_TYPE=$(echo $JOB_NAME | awk -F '-' '{ print $5 }')
+mkdir -p /tmp/sahara/$CONFIG_PATH
+cp $WORKSPACE/$CONFIG_PATH/sahara-test-config-vanilla-2.6.yaml /tmp/sahara/$CONFIG_PATH
+cp $WORKSPACE/$CONFIG_PATH/sahara-test-config-spark.yaml /tmp/sahara/$CONFIG_PATH
+
 cd /tmp/sahara
 # prepare test dependencies
-tox -e integration --notest
+if [[ "$JOB_NAME" =~ scenario ]]; then
+  tox -e scenario --notest
+  .tox/scenario/bin/pip install $WORKSPACE
+else
+  tox -e integration --notest
+  .tox/integration/bin/pip install $WORKSPACE
+fi
 
-# change sahara-client
-.tox/integration/bin/pip install $WORKSPACE
 
 bash -x /tmp/sahara-ci-config/slave-scripts/gate-sahara.sh /tmp/sahara
