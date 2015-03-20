f2a614f56c5e7cba1234de626cc9050192916ad0
Denis Egorenko <degorenko@mirantis.com>
2015-03-20 15:12:51 +0400

    Run all jobs with scenario tests
    All jobs for master branch will be run with scenario
    tests now. For stable/juno branch, jobs will be run with old
    integration tests
    
    Change-Id: Iec0d37ea92d77559f7b424361a58b249533734ac

--- slave-scripts/gate-saharaclient.sh
+++ slave-scripts/gate-saharaclient.sh
@@ -2,15 +2,13 @@
 
 git clone https://git.openstack.org/openstack/sahara /tmp/sahara
 cd /tmp/sahara
-if [ "$ZUUL_BRANCH" != "master" ]; then
-   git checkout "$ZUUL_BRANCH"
-   sudo pip install -U -r requirements.txt
-fi
 
-if [[ "$JOB_NAME" =~ scenario ]]; then
+if [ "$ZUUL_BRANCH" == "master" ]; then
   tox -e scenario --notest
   .tox/scenario/bin/pip install $WORKSPACE
 else
+  git checkout "$ZUUL_BRANCH"
+  sudo pip install -U -r requirements.txt
   tox -e integration --notest
   .tox/integration/bin/pip install $WORKSPACE
 fi
