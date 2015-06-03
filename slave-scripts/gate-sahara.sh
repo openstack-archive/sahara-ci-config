4bee52855638e7d34739facfb1469f23dab1284c
Denis Egorenko <degorenko@mirantis.com>
2015-06-03 18:35:36 +0300

    Rename all plugins to full plugin version
    Rename short plugin names to full plugin names and add
    OS version to job name.
    
    Also sort by alphabetical functions-* files.
    
    Change-Id: I5155613715fd2ced99034807f14f495487d2d8ac

--- slave-scripts/gate-sahara.sh
+++ slave-scripts/gate-sahara.sh
@@ -9,69 +9,50 @@ CLUSTER_HASH=${CLUSTER_HASH:-$RANDOM}
 cluster_name="$HOST-$ZUUL_CHANGE-$CLUSTER_HASH"
 
 SAHARA_PATH=${1:-$WORKSPACE}
-sahara_conf_path=$SAHARA_PATH/etc/sahara/sahara.conf
+sahara_conf_file=$SAHARA_PATH/etc/sahara/sahara.conf
 sahara_templates_path=$SAHARA_PATH/etc/scenario/sahara-ci
 
-job_type=$(echo $JOB_NAME | awk -F '-' '{ print $5 }')
 engine_type=$(echo $JOB_NAME | awk -F '-' '{ print $4 }')
+plugin=$(echo $JOB_NAME | awk -F '-' '{ print $5 }')
+os=$(echo $JOB_NAME | awk -F '-' '{ print $6 }')
+image_name=${plugin}_${os}
+mode="aio"
+sahara_plugin=$(echo $plugin | awk -F '_' '{ print $1 } ')
 
-# Image names
-hdp_two_image=sahara_hdp_2_latest
-vanilla_two_six_image=ubuntu_vanilla_2.6_latest
-spark_image=sahara_spark_latest
-cdh_5_3_0_centos_image=centos_cdh_latest
-cdh_5_3_0_ubuntu_image=ubuntu_cdh_latest
-cdh_5_4_0_image=ubuntu_cdh_5.4.0_latest
-mapr_402mrv2_image=ubuntu_mapr_latest
-
-case $job_type in
-    hdp_2)
-       DISTRIBUTE_MODE=True
-       tests_config_file="$sahara_templates_path/hdp-2.0.6.yaml"
-       insert_scenario_value $tests_config_file hdp_two_image
+case $plugin in
+    hdp_2.0.6)
+       mode=distribute
+       scenario_conf_file="$sahara_templates_path/hdp-2.0.6.yaml"
        ;;
-    vanilla_2.6)
-       DISTRIBUTE_MODE=True
-       tests_config_file="$sahara_templates_path/vanilla-2.6.0.yaml"
-       insert_scenario_value $tests_config_file vanilla_two_six_image
+    vanilla_2.6.0)
+       mode=distribute
+       scenario_conf_file="$sahara_templates_path/vanilla-2.6.0.yaml"
        ;;
     transient)
+       # transient is using image with latest vanilla version
+       image_name=vanilla_2.6.0_u14
        concurrency=3
-       DISTRIBUTE_MODE=True
-       tests_config_file="$sahara_templates_path/transient.yaml"
-       insert_scenario_value $tests_config_file vanilla_two_six_image
+       mode=distribute
+       scenario_conf_file="$sahara_templates_path/transient.yaml"
        ;;
-    cdh_5.3.0*)
-       insert_config_value $sahara_conf_path DEFAULT plugins cdh
-       if [[ "$job_type" =~ centos ]]; then
-          cdh_image=$cdh_5_3_0_centos_image
-       else
-          cdh_image=$cdh_5_3_0_ubuntu_image
-       fi
-       tests_config_file="$sahara_templates_path/cdh-5.3.0.yaml"
-       insert_scenario_value $tests_config_file cdh_image
+    cdh_5.3.0)
+       scenario_conf_file="$sahara_templates_path/cdh-5.3.0.yaml"
        ;;
     cdh_5.4.0)
-       insert_config_value $sahara_conf_path DEFAULT plugins cdh
-       tests_config_file="$sahara_templates_path/cdh-5.4.0.yaml"
-       insert_scenario_value $tests_config_file cdh_5_4_0_image
+       scenario_conf_file="$sahara_templates_path/cdh-5.4.0.yaml"
     ;;
-    spark)
-       insert_config_value $sahara_conf_path DEFAULT plugins spark
-       tests_config_file="$sahara_templates_path/spark-1.0.0.yaml"
-       insert_scenario_value $tests_config_file spark_image
+    spark_1.0.0)
+       scenario_conf_file="$sahara_templates_path/spark-1.0.0.yaml"
        ;;
-    mapr)
-       insert_config_value $sahara_conf_path DEFAULT plugins mapr
-       DISTRIBUTE_MODE=True
-       tests_config_file="$sahara_templates_path/mapr-4.0.2.mrv2.yaml"
-       insert_scenario_value $tests_config_file mapr_402mrv2_image
+    mapr_4.0.2.mrv2)
+       mode=distribute
+       scenario_conf_file="$sahara_templates_path/mapr-4.0.2.mrv2.yaml"
        ;;
 esac
 
 sudo pip install . --no-cache-dir
 enable_pypi
-write_sahara_main_conf "$sahara_conf_path" "$engine_type"
-write_tests_conf "$tests_config_file" "$cluster_name"
-start_sahara "$sahara_conf_path" && run_tests "$tests_config_file" "$concurrency"
+write_sahara_main_conf "$sahara_conf_file" "$engine_type" "$sahara_plugin"
+write_tests_conf "$scenario_conf_file" "$cluster_name" "$image_name"
+start_sahara "$sahara_conf_file" "$mode" && run_tests "$scenario_conf_file" "$concurrency"
 print_python_env
