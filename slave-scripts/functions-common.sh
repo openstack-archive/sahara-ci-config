c53e86c4e808018473fd72d99b36edc7a705e6bb
Denis Egorenko <degorenko@mirantis.com>
2015-06-03 18:35:36 +0300

    Rename all plugins to full plugin version
    Rename short plugin names to full plugin names and add
    OS version to job name
    
    Change-Id: I5155613715fd2ced99034807f14f495487d2d8ac

--- slave-scripts/functions-common.sh
+++ slave-scripts/functions-common.sh
@@ -1,14 +1,8 @@
 #!/bin/bash -xe
 
-sahara_configs_path=$WORKSPACE/sahara-ci-config/config/sahara
+configs_path=$WORKSPACE/sahara-ci-config/config
 ci_flavor_id=\'20\'
 medium_flavor_id=\'3\'
-
-enable_pypi() {
-  mkdir -p ~/.pip
-  export PIP_USE_MIRRORS=True
-  cp $sahara_configs_path/pip.conf ~/.pip/pip.conf
-}
 
 conf_has_option() {
   local file=$1
@@ -18,6 +12,19 @@ conf_has_option() {
 
   line=$(sed -ne "/^\[$section\]/,/^\[.*\]/ { /^$option[ \t]*=/ p; }" "$file")
   [ -n "$line" ]
+}
+
+enable_pypi() {
+  mkdir -p ~/.pip
+  export PIP_USE_MIRRORS=True
+  cp $configs_path/pip.conf ~/.pip/pip.conf
+}
+
+failure() {
+  local reason=$1
+  print_python_env
+  echo "$reason"
+  exit 1
 }
 
 insert_config_value() {
@@ -50,46 +57,33 @@ insert_scenario_value() {
   sed -i "s/%${value}%/${!value}/g" $config
 }
 
-write_sahara_main_conf() {
-  local conf_path=$1
-  local engine=$2
-  insert_config_value $conf_path DEFAULT infrastructure_engine $engine
-  insert_config_value $conf_path DEFAULT api_workers 4
-  insert_config_value $conf_path DEFAULT use_identity_api_v3 true
-  insert_config_value $conf_path DEFAULT use_neutron $USE_NEUTRON
-  insert_config_value $conf_path DEFAULT min_transient_cluster_active_time 30
-  insert_config_value $conf_path DEFAULT node_domain ci
-  insert_config_value $conf_path database connection mysql://sahara-citest:sahara-citest@localhost/sahara?charset=utf8
-  insert_config_value $conf_path keystone_authtoken auth_uri http://$OPENSTACK_HOST:5000/v2.0/
-  insert_config_value $conf_path keystone_authtoken identity_uri http://$OPENSTACK_HOST:35357/
-  insert_config_value $conf_path keystone_authtoken admin_user $OS_USERNAME
-  insert_config_value $conf_path keystone_authtoken admin_password $OS_PASSWORD
-  insert_config_value $conf_path keystone_authtoken admin_tenant_name $OS_TENANT_NAME
-
-  echo "----------- sahara.conf -----------"
-  cat $conf_path
-  echo "--------------- end ---------------"
-}
-
 print_python_env() {
   [ -f $SAHARA_PATH/.tox/integration/bin/pip ] && $SAHARA_PATH/.tox/integration/bin/pip freeze > $WORKSPACE/logs/python-integration-env.txt
   [ -f $SAHARA_PATH/.tox/scenario/bin/pip ] && $SAHARA_PATH/.tox/scenario/bin/pip freeze > $WORKSPACE/logs/python-scenario-env.txt
   pip freeze > $WORKSPACE/logs/python-system-env.txt
 }
 
-failure() {
-  local reason=$1
-  print_python_env
-  echo "$reason"
-  exit 1
+run_tests() {
+  local config=$1
+  local concurrency=${2:-"1"}
+  echo "Integration tests are started"
+  export PYTHONUNBUFFERED=1
+  local scenario_credentials=$(dirname $1)/credentials.yaml
+  local scenario_edp=$(dirname $1)/edp.yaml
+  # Temporary use additional log file, due to wrong status code from tox scenario tests
+  # tox -e scenario $scenario_common $config || failure "Integration tests are failed"
+  tox -e scenario $scenario_credentials $scenario_edp $config | tee tox.log
+  STATUS=$(grep "\ -\ Failed" tox.log | awk '{print $3}')
+  if [ "$STATUS" != "0" ]; then failure "Integration tests have failed"; fi
 }
 
 start_sahara() {
   local conf_path=$1
   local conf_dir=$(dirname $1)
+  local mode=$2
   mkdir $WORKSPACE/logs
   sahara-db-manage --config-file $conf_path  upgrade head || failure "Command 'sahara-db-manage' failed"
-  if [ "$DISTRIBUTE_MODE" == "True" ]; then
+  if [ "$mode" == "distribute" ]; then
     screen -dmS sahara-api /bin/bash -c "PYTHONUNBUFFERED=1 sahara-api --config-dir $conf_dir -d --log-file $WORKSPACE/logs/sahara-log-api.txt"
     sleep 2
     screen -dmS sahara-engine_1 /bin/bash -c "PYTHONUNBUFFERED=1 sahara-engine --config-dir $conf_dir -d --log-file $WORKSPACE/logs/sahara-log-engine-1.txt"
@@ -102,6 +96,29 @@ start_sahara() {
   if ! timeout ${api_responding_timeout} sh -c "while ! curl -s http://127.0.0.1:8386/v1.1/ 2>/dev/null | grep -q 'Authentication required' ; do sleep 1; done"; then
     failure "Sahara API failed to respond within ${api_responding_timeout} seconds"
   fi
+}
+
+write_sahara_main_conf() {
+  local conf_path=$1
+  local engine=$2
+  local plugin=$3
+  insert_config_value $conf_path DEFAULT infrastructure_engine $engine
+  insert_config_value $conf_path DEFAULT api_workers 4
+  insert_config_value $conf_path DEFAULT use_identity_api_v3 true
+  insert_config_value $conf_path DEFAULT use_neutron $USE_NEUTRON
+  insert_config_value $conf_path DEFAULT min_transient_cluster_active_time 30
+  insert_config_value $conf_path DEFAULT node_domain ci
+  insert_config_value $conf_path DEFAULT plugins $plugin
+  insert_config_value $conf_path database connection mysql://sahara-citest:sahara-citest@localhost/sahara?charset=utf8
+  insert_config_value $conf_path keystone_authtoken auth_uri http://$OPENSTACK_HOST:5000/v2.0/
+  insert_config_value $conf_path keystone_authtoken identity_uri http://$OPENSTACK_HOST:35357/
+  insert_config_value $conf_path keystone_authtoken admin_user $OS_USERNAME
+  insert_config_value $conf_path keystone_authtoken admin_password $OS_PASSWORD
+  insert_config_value $conf_path keystone_authtoken admin_tenant_name $OS_TENANT_NAME
+
+  echo "----------- sahara.conf -----------"
+  cat $conf_path
+  echo "--------------- end ---------------"
 }
 
 write_tests_conf() {
@@ -126,18 +143,4 @@ write_tests_conf() {
   echo "----------- tests config -----------"
   cat $test_conf
   echo "---------------- end ---------------"
-}
-
-run_tests() {
-  local config=$1
-  local concurrency=${2:-"1"}
-  echo "Integration tests are started"
-  export PYTHONUNBUFFERED=1
-  local scenario_credentials=$(dirname $1)/credentials.yaml
-  local scenario_edp=$(dirname $1)/edp.yaml
-  # Temporary use additional log file, due to wrong status code from tox scenario tests
-  # tox -e scenario $scenario_common $config || failure "Integration tests are failed"
-  tox -e scenario $scenario_credentials $scenario_edp $config | tee tox.log
-  STATUS=$(grep "\ -\ Failed" tox.log | awk '{print $3}')
-  if [ "$STATUS" != "0" ]; then failure "Integration tests have failed"; fi
 }
--- slave-scripts/gate-sahara.sh
+++ slave-scripts/gate-sahara.sh
@@ -9,79 +9,65 @@ CLUSTER_HASH=${CLUSTER_HASH:-$RANDOM}
 cluster_name="$HOST-$ZUUL_CHANGE-$CLUSTER_HASH"
 
 SAHARA_PATH=${1:-$WORKSPACE}
-sahara_conf_path=$SAHARA_PATH/etc/sahara/sahara.conf
+sahara_conf_file=$SAHARA_PATH/etc/sahara/sahara.conf
 sahara_templates_path=$SAHARA_PATH/etc/scenario/sahara-ci
 
-job_type=$(echo $JOB_NAME | awk -F '-' '{ print $5 }')
 engine_type=$(echo $JOB_NAME | awk -F '-' '{ print $4 }')
+plugin=$(echo $JOB_NAME | awk -F '-' '{ print $5 }')
+os=$(echo $JOB_NAME | awk -F '-' '{ print $6 }')
+${plugin//./_}_image=${plugin}_${os}_latest
+mode="aio"
+sahara_plugin=$(echo $plugin | awk -F '_' '{ print $1 } ')
 
-# Image names
-hdp_image=sahara_hdp_1_latest
-hdp_two_image=sahara_hdp_2_latest
-vanilla_image=ubuntu_vanilla_1_latest
-vanilla_two_six_image=ubuntu_vanilla_2.6_latest
-spark_image=sahara_spark_latest
-cdh_5_3_0_centos_image=centos_cdh_latest
-cdh_5_3_0_ubuntu_image=ubuntu_cdh_latest
-cdh_5_4_0_image=ubuntu_cdh_5.4.0_latest
-mapr_402mrv2_image=ubuntu_mapr_latest
-
-case $job_type in
-    hdp_1)
-       tests_config_file="$sahara_templates_path/hdp-1.3.2.yaml"
-       insert_scenario_value $tests_config_file hdp_image
+case $plugin in
+    hdp_1.3.2)
+       scenario_conf_file="$sahara_templates_path/hdp-1.3.2.yaml"
+       insert_scenario_value $scenario_conf_file hdp_1_3_2_image
        ;;
-    hdp_2)
-       DISTRIBUTE_MODE=True
-       tests_config_file="$sahara_templates_path/hdp-2.0.6.yaml"
-       insert_scenario_value $tests_config_file hdp_two_image
+    hdp_2.0.6)
+       mode=distribute
+       scenario_conf_file="$sahara_templates_path/hdp-2.0.6.yaml"
+       insert_scenario_value $scenario_conf_file hdp_2_0_6_image
        ;;
-    vanilla_1)
-       tests_config_file="$sahara_templates_path/vanilla-1.2.1.yaml"
-       insert_scenario_value $tests_config_file vanilla_image
+    vanilla_1.2.1)
+       scenario_conf_file="$sahara_templates_path/vanilla-1.2.1.yaml"
+       insert_scenario_value $scenario_conf_file vanilla_1_2_1_image
        ;;
-    vanilla_2.6)
-       DISTRIBUTE_MODE=True
-       tests_config_file="$sahara_templates_path/vanilla-2.6.0.yaml"
-       insert_scenario_value $tests_config_file vanilla_two_six_image
+    vanilla_2.6.0)
+       mode=distribute
+       scenario_conf_file="$sahara_templates_path/vanilla-2.6.0.yaml"
+       insert_scenario_value $scenario_conf_file vanilla_2_6_0_image
        ;;
     transient)
+       # transient is using image with latest vanilla version
+       transient_image=vanilla_2.6.0_u14
        concurrency=3
-       DISTRIBUTE_MODE=True
-       tests_config_file="$sahara_templates_path/transient.yaml"
-       insert_scenario_value $tests_config_file vanilla_two_six_image
+       mode=distribute
+       scenario_conf_file="$sahara_templates_path/transient.yaml"
+       insert_scenario_value $scenario_conf_file transient_image
        ;;
-    cdh_5.3.0*)
-       insert_config_value $sahara_conf_path DEFAULT plugins cdh
-       if [[ "$job_type" =~ centos ]]; then
-          cdh_image=$cdh_5_3_0_centos_image
-       else
-          cdh_image=$cdh_5_3_0_ubuntu_image
-       fi
-       tests_config_file="$sahara_templates_path/cdh-5.3.0.yaml"
-       insert_scenario_value $tests_config_file cdh_image
+    cdh_5.3.0)
+       scenario_conf_file="$sahara_templates_path/cdh-5.3.0.yaml"
+       insert_scenario_value $scenario_conf_file cdh_5_3_0_image
        ;;
     cdh_5.4.0)
-       insert_config_value $sahara_conf_path DEFAULT plugins cdh
-       tests_config_file="$sahara_templates_path/cdh-5.4.0.yaml"
-       insert_scenario_value $tests_config_file cdh_5_4_0_image
+       scenario_conf_file="$sahara_templates_path/cdh-5.4.0.yaml"
+       insert_scenario_value $scenario_conf_file cdh_5_4_0_image
     ;;
-    spark)
-       insert_config_value $sahara_conf_path DEFAULT plugins spark
-       tests_config_file="$sahara_templates_path/spark-1.0.0.yaml"
-       insert_scenario_value $tests_config_file spark_image
+    spark_1.0.0)
+       scenario_conf_file="$sahara_templates_path/spark-1.0.0.yaml"
+       insert_scenario_value $scenario_conf_file spark_1_0_0_image
        ;;
-    mapr)
-       insert_config_value $sahara_conf_path DEFAULT plugins mapr
-       DISTRIBUTE_MODE=True
-       tests_config_file="$sahara_templates_path/mapr-4.0.2.mrv2.yaml"
-       insert_scenario_value $tests_config_file mapr_402mrv2_image
+    mapr_4.0.2.mrv2)
+       mode=distribute
+       scenario_conf_file="$sahara_templates_path/mapr-4.0.2.mrv2.yaml"
+       insert_scenario_value $scenario_conf_file mapr_4_0_2_mrv2_image
        ;;
 esac
 
 sudo pip install . --no-cache-dir
 enable_pypi
-write_sahara_main_conf "$sahara_conf_path" "$engine_type"
-write_tests_conf "$tests_config_file" "$cluster_name"
-start_sahara "$sahara_conf_path" && run_tests "$tests_config_file" "$concurrency"
+write_sahara_main_conf "$sahara_conf_file" "$engine_type" "$sahara_plugin"
+write_tests_conf "$scenario_conf_file" "$cluster_name"
+start_sahara "$sahara_conf_file" "$mode" && run_tests "$scenario_conf_file" "$concurrency"
 print_python_env
