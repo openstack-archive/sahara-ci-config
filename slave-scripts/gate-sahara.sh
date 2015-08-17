2afbd2fac0beba88779a6503fcbc214951af3431
Evgeny Sikachev <esikachev@mirantis.com>
2015-08-17 13:43:44 +0300

    Update version of vanilla plugin for transient check
    Transient is using image with latest vanilla version
    
    Change-Id: Ib05a8b17487f42c4f3017211de5decbbc9af75ef

--- slave-scripts/gate-sahara.sh
+++ slave-scripts/gate-sahara.sh
@@ -37,12 +37,16 @@ case $plugin in
     ;;
     transient)
        # transient is using image with latest vanilla version
-       image_name=vanilla_2.6.0_u14
+       if [ "$ZUUL_BRANCH" == "stable/kilo" ]; then
+        image_name=vanilla_2.6.0_u14
+        template_image_prefix="vanilla_two_six"
+       fi
+       image_name=vanilla_2.7.1_u14
        sahara_plugin=vanilla
        concurrency=3
        mode=distribute
        scenario_conf_file="$sahara_templates_path/transient.yaml.mako"
-       template_image_prefix="vanilla_two_six"
+       template_image_prefix="vanilla_two_seven_one"
        ;;
     cdh_5.3.0)
        scenario_conf_file="$sahara_templates_path/cdh-5.3.0.yaml.mako"
