990beee6d8c925ae0cc6112520fc278b9d610d07
Sergey Reshetnyak <sreshetniak@mirantis.com>
2015-08-31 12:43:32 +0300

    Change path to ambari scenario file
    Now in Sahara we have ambari-2.3.yaml.mako scenario which creates
    HDP 2.3 stack with Ambari 2.1
    
    Change-Id: I0692c54064a1d11d3a5928038ab4688560d973c3

--- slave-scripts/dib.sh
+++ slave-scripts/dib.sh
@@ -110,7 +110,8 @@ case $plugin in
        env ambari_${os_type}_image_name=${ambari_2_1_image} SIM_REPO_PATH=$WORKSPACE tox -e venv -- sahara-image-create -p ambari -i $os_type -v 2.1.0
        check_error_code $? ${ambari_2_1_image}.qcow2
        upload_image "${plugin}" "${username}" ${ambari_2_1_image}
-       scenario_conf_file="$sahara_templates_path/ambari-2.1.yaml.mako"
+       # we use Ambari 2.1 management console for creating HDP 2.3 stack
+       scenario_conf_file="$sahara_templates_path/ambari-2.3.yaml.mako"
        template_image_prefix="ambari_2_1"
     ;;
 
