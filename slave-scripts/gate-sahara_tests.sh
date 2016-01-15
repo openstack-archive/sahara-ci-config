77fc3dbe1b49caedca16d24ea310a23d061168ba
Evgeny Sikachev <esikachev@mirantis.com>
2016-01-15 17:09:53 +0300

    Add sahara-ci to sahara-scenario
    Add sahara-scenario to zuul/layout.yaml
    
    Change-Id: Ie8756a7547fd1405030f2a417d69d1563338e126

---
+++ slave-scripts/gate-sahara_tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -xe
+
+. $FUNCTION_PATH/functions-common.sh
+
+plugin=$(echo $JOB_NAME | awk -F '-' '{ print $5 }')
+
+case $plugin in
+    vanilla_2.6.0)
+       $ZUUL_BRANCH="stable/kilo"
+       ;;
+    cdh_5.3.0)
+       $ZUUL_BRANCH="stable/kilo"
+       ;;
+    spark_1.0.0)
+       $ZUUL_BRANCH="stable/kilo"
+       ;;
+esac
+
+sahara_path="/tmp/sahara"
+get_dependency "$sahara_path" "openstack/sahara" "$ZUUL_BRANCH"
+cd "$sahara_path"
+
+tox -e venv --notest
+.tox/venv/bin/pip install $WORKSPACE
+
+$WORKSPACE/sahara-ci-config/slave-scripts/gate-sahara.sh "$sahara_path" "$WORKSPACE"
