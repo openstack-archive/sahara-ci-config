b59e5b89649b63ec27445e9775d8ea79f63cb179
Evgeny Sikachev <esikachev@mirantis.com>
2016-01-26 15:41:39 +0300

    Fix ci in saharaclient
    Now "scenario" env is not exist
    
    Change-Id: I2fcf01fa7f79f7d0bbe35b899724619d7074142b

--- slave-scripts/gate-saharaclient.sh
+++ slave-scripts/gate-saharaclient.sh
@@ -6,7 +6,7 @@ sahara_path="/tmp/sahara"
 get_dependency "$sahara_path" "openstack/sahara" "$ZUUL_BRANCH"
 cd "$sahara_path"
 
-tox -e scenario --notest
-.tox/scenario/bin/pip install $WORKSPACE
+tox -e venv --notest
+.tox/venv/bin/pip install $WORKSPACE
 
 $WORKSPACE/sahara-ci-config/slave-scripts/gate-sahara.sh "$sahara_path"
