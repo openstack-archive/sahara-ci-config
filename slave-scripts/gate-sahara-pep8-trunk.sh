94cb7df6da7c9462a9c0093f8a546ee334e2fa9a
Vitaly Gridnev <vgridnev@mirantis.com>
2016-02-09 11:07:00 +0300

    Use upstream bashate in pep8 trunk job
    Change-Id: I1b3e16c036c25721b1cf64af02fe35b0770ac85a

--- slave-scripts/gate-sahara-pep8-trunk.sh
+++ slave-scripts/gate-sahara-pep8-trunk.sh
@@ -2,10 +2,13 @@
 
 set -o pipefail
 
-# replace hacking with master tarball
+# replace hacking and bashate with master tarball
 sed -i '/^hacking/d' test-requirements.txt
+sed -i '/^bashate/d' test-requirements.txt
+
 REQS=$(cat test-requirements.txt)
-echo -e "-f http://tarballs.openstack.org/hacking/hacking-master.tar.gz#egg=hacking-master\nhacking==master\n${REQS}" > test-requirements.txt
+echo -e "-f http://tarballs.openstack.org/bashate/bashate-master.tar.gz#egg=bashate-master\nbashate==master\n\
+-f http://tarballs.openstack.org/hacking/hacking-master.tar.gz#egg=hacking-master\nhacking==master\n${REQS}" > test-requirements.txt
 
 sed -i '/^ignore/d' tox.ini
 sed -ie 's/\(^exclude.*\)/\1,*sahara-ci-config*/' tox.ini
