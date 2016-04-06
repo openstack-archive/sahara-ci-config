130202ffac1ebad3b7d45532429060239b278d2b
Evgeny Sikachev <esikachev@mirantis.com>
2016-04-06 14:23:19 +0300

    Add jobs for mitaka in sahara-tests
    Change-Id: Ia6f84527ad324838e3e07d314fed85e1b443c8c2

--- slave-scripts/gate-sahara_tests.sh
+++ slave-scripts/gate-sahara_tests.sh
@@ -3,22 +3,28 @@
 . $FUNCTION_PATH/functions-common.sh
 
 plugin=$(echo $JOB_NAME | awk -F '-' '{ print $5 }')
+release=$(echo $JOB_NAME | awk -F '-' '{ print $7 }')
 
-if [ $(echo $JOB_NAME | awk -F '-' '{ print $7 }') == 'liberty' ]; then
-    ZUUL_BRANCH="stable/liberty"
-else
-    case $plugin in
-        vanilla_2.6.0)
-            ZUUL_BRANCH="stable/kilo"
-            ;;
-        cdh_5.3.0)
-            ZUUL_BRANCH="stable/kilo"
-            ;;
-        spark_1.0.0)
-            ZUUL_BRANCH="stable/kilo"
-            ;;
-    esac
-fi
+case $release in
+    liberty)
+        ZUUL_BRANCH="stable/liberty"
+	;;
+    mitaka)
+        ZUUL_BRANCH="stable/mitaka"
+	;;
+    *)
+        case $plugin in
+            vanilla_2.6.0)
+                ZUUL_BRANCH="stable/kilo"
+                ;;
+            cdh_5.3.0)
+                ZUUL_BRANCH="stable/kilo"
+                ;;
+            spark_1.0.0)
+                ZUUL_BRANCH="stable/kilo"
+                ;;
+        esac
+esac
 
 sahara_path="/tmp/sahara"
 get_dependency "$sahara_path" "openstack/sahara" "$ZUUL_BRANCH"
