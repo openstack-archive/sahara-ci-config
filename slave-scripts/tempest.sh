272a83f2a64923284ee8f61fc17b9a39998852bf
Luigi Toscano <ltoscano@redhat.com>
2016-04-11 16:54:25 +0200

    Install Sahara-tests in the Tempest venv, run all tests
    Sahara-tests plugins can then be used by Tempest.
    Also, all tests which matches 'data_processing' are executed.
    
    Change-Id: I02fd2aea9d239231b785e1d8216fd380ac994f82

--- slave-scripts/tempest.sh
+++ slave-scripts/tempest.sh
@@ -8,6 +8,7 @@
 project=$(echo $JOB_NAME | awk -F '-' '{ print $2 }')
 image_id=$(glance image-list | grep ubuntu-test-image | awk '{print $2}')
 
+SAHARATESTS_REPO=https://git.openstack.org/openstack/sahara-tests
 if [ "$project" == "sahara" ]; then
    SAHARA_PATH="$WORKSPACE"
    SAHARACLIENT_PATH=/tmp/saharaclient
@@ -82,9 +83,10 @@ fi
 export OS_TEST_TIMEOUT=5400
 tox -e $TOXENV --notest
 .tox/$TOXENV/bin/pip install $SAHARACLIENT_PATH/.
+.tox/$TOXENV/bin/pip install git+$SAHARATESTS_REPO
 # Temporary use additional log file, due to wrong status code from tox scenario tests
 # tox -e $TOXENV -- tempest.scenario.data_processing.client_tests || failure "Tempest tests are failed"
-tox -e $TOXENV -- tempest.scenario.data_processing.client_tests | tee tox.log
+tox -e $TOXENV -- '.*data_processing.*' | tee tox.log
 STATUS=$(grep "\ -\ Failed" tox.log | awk '{print $3}')
 if [ "$STATUS" != "0" ]; then failure "Tempest tests have failed"; fi
 .tox/$TOXENV/bin/pip freeze > $WORKSPACE/logs/python-tempest-env.txt
