d0865144015b4f69919754f8bf86d155dd205bf6
Evgeny Sikachev <esikachev@mirantis.com>
2016-04-13 17:26:23 +0300

    Fix variables with host credentials
    Update variables with tenant id and host ip
    
    Change-Id: I9d55c23293cac9defda4b3b2f98823b86ceacd26

--- slave-scripts/integration-cleanup.sh
+++ slave-scripts/integration-cleanup.sh
@@ -9,13 +9,13 @@ job_type=$(echo $PREV_JOB | awk -F '-' '{ print $1 }')
 export os_username=$os_username
 export os_password=$os_password
 export os_tenant_name=$os_tenant_name
-if [[ "$HOST_NAME" =~ 42 ]]; then
-    export os_auth_url="http://$OPENSTACK_HOST_42_LAB:5000/v2.0"
-    export os_image_endpoint="http://$OPENSTACK_42_NEUTRON_LAB:8004/v1/$LAB_42_TENANT_ID"
+if [[ "$HOST_NAME" =~ trusty-42 ]]; then
+    export os_auth_url="http://$OPENSTACK_HOST_LAB_42:5000/v2.0"
+    export os_image_endpoint="http://$OPENSTACK_LAB_42:8004/v1/$LAB_42_TENANT_ID"
     host="c1"
 else
-    export os_auth_url="http://$OPENSTACK_HOST_43_LAB:5000/v2.0"
-    export os_image_endpoint="http://$OPENSTACK_HOST_43_LAB:8004/v1/$LAB_43_TENANT_ID"
+    export os_auth_url="http://$OPENSTACK_HOST_LAB_43:5000/v2.0"
+    export os_image_endpoint="http://$OPENSTACK_HOST_LAB_43:8004/v1/$LAB_43_TENANT_ID"
     host="c2"
 fi
 if [[ $(echo $PREV_JOB | awk -F '-' '{ print $2 }') =~ ui ]]; then
