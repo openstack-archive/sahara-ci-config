6d55b5f03a49605740193e0a604b7e3d0cce05ce
Evgeny Sikachev <esikachev@mirantis.com>
2016-06-07 20:15:15 +0300

    Added publisher script and job
    Added job for daily publishing logs from slaves
    
    Change-Id: Ie2733961928bb768003dd64cb30a5eb18c3b2302

---
+++ slave-scripts/publish-logs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+# Example of $JENKINS_HOME/credentials:
+#OPENSTACK_HOST_LAB_42=172.18.168.42
+#OPENSTACK_HOST_LAB_43=172.18.168.43
+#REPORT_HOST_SSH_USERNAME=user
+source $JENKINS_HOME/credentials
+
+dir_name=$(date '+%d-%b-%Y')
+mkdir $dir_name
+
+echo "Moving logs from $OPENSTACK_HOST_LAB_42 to the https://sahara-ci-reports.vm.mirantis.net"
+ssh -i ~/lab_key $LAB_42_SSH_USERNAME@$OPENSTACK_HOST_LAB_42 "rsync -e "ssh -i .ssh/rsync_key" -a /opt/stack/logs/ $REPORT_HOST_SSH_USERNAME@sahara-ci-reports.vm.mirantis.net:/var/www/html/devstack/42/$dir_name"
+rm $dir_name/*
+
+echo "Moving logs from $OPENSTACK_HOST_LAB_43 to the https://sahara-ci-reports.vm.mirantis.net"
+ssh -i ~/lab_key $LAB_43_SSH_USERNAME@$OPENSTACK_HOST_LAB_43 "rsync -e "ssh -i .ssh/rsync_key" -a /opt/stack/logs/ $REPORT_HOST_SSH_USERNAME@sahara-ci-reports.vm.mirantis.net:/var/www/html/devstack/43/$dir_name"
+
+rm -rf $dir_name
