a212a9d09528a4ff8a3524e62817eed2691ef21a
Michael Ionkin <mionkin@mirantis.com>
2016-06-20 13:18:16 +0300

    Jobs for CDH 5.7 and centos 7
    Jobs added for sahara and sahara-image-elements
    
    Change-Id: I9bddac16ef475f2d9f421b6ef83788c451488bd5

--- slave-scripts/functions-dib.sh
+++ slave-scripts/functions-dib.sh
@@ -68,8 +68,11 @@ upload_image() {
            cdh_5.4.0)
              image_properties="--property _sahara_tag_5.4.0=True --property _sahara_tag_cdh=True --property _sahara_username=${username}"
            ;;
-	   cdh_5.5.0)
+	       cdh_5.5.0)
              image_properties="--property _sahara_tag_5.5.0=True --property _sahara_tag_cdh=True --property _sahara_username=${username}"
+           ;;
+           cdh_5.7.0)
+             image_properties="--property _sahara_tag_5.7.0=True --property _sahara_tag_cdh=True --property _sahara_username=${username}"
            ;;
            spark_1.0.0)
              image_properties="--property _sahara_tag_spark=True --property _sahara_tag_1.0.0=True --property _sahara_username=${username}"
