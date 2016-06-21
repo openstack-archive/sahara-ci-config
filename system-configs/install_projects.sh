a37987297b075c34d29f143cea00ca3c64c71712
Evgeny Sikachev <esikachev@mirantis.com>
2016-06-21 15:48:02 +0300

    [wip] scripts for migration
    Change-Id: Ic218243efeb6c6aae17faa665755c5e722e10e2b

---
+++ system-configs/install_projects.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xe
+
+. /home/ubuntu/sahara-ci-config/system-configs/functions.sh
+
+default_path=/opt/ci
+projects=( "openstack-infra/nodepool" "openstack-infra/zuul" "openstack-infra/jenkins-job-builder" )
+
+for project in "${projects[@]}"
+do
+    project_dir=$default_path/$(basename $project)
+    project_conf_dir=/etc/$(basename $project)
+    mkdir $project_conf_dir
+    touch $project_conf_dir/$(basename $project).conf
+    clone $project $project_dir
+    install_to_venv $project_dir
+done
+
+# prepare zuul
+
+#prepare nodepool
+cp  /home/ubuntu/sahara-ci-config/config/nodpool/configs/* /etc/nodepool
