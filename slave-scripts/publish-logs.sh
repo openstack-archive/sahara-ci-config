c0a4995467542a8d70f4a9b5fb129e9eeec44d06
Evgeny Sikachev <esikachev@mirantis.com>
2016-06-24 11:38:17 +0300

    Added publisher script and job
    Added job for daily publishing logs from slaves
    
    Change-Id: Ie2733961928bb768003dd64cb30a5eb18c3b2302

---
+++ slave-scripts/publish-logs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+dir_name=$(date '+%d-%b-%Y')
+lab=$(echo $JOB_NAME | awk -F '-' '{ print $4 }')
+
+echo "Moving logs to the https://sahara-ci-reports.mirantis.com"
+rsync -e "ssh -i ~/.ssh/rsync_key" -a /opt/stack/logs/ ubuntu@172.18.180.78:/var/www/html/devstack/$lab/$dir_name
