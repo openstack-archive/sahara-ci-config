de059ed549777b1e1828adc3c68f95ea0724f44d
Alina Nesterova <zemuvier@gmail.com>
2016-07-07 16:58:00 +0300

    Added storm 1.0.1 plugin
    Depends-on: I5f1ba04ea69e6dd467342e7f60305d2aacc2b725
    
    Change-Id: I4bf9f5940754e714810e2d93b1b6ae573a518c9f

--- slave-scripts/dib.sh
+++ slave-scripts/dib.sh
@@ -125,6 +125,13 @@ case $plugin in
        scenario_conf_file="$sahara_templates_path/mapr-5.1.0.mrv2.yaml.mako"
        template_image_prefix="mapr_510mrv2"
     ;;
+    storm_1.0.1)
+       env ubuntu_spark_image_name=${storm_1_0_1_image} SIM_REPO_PATH=$WORKSPACE tox -e venv -- sahara-image-create -p storm -v 1.0.1
+       check_error_code $? ${storm_1_0_1_image}.qcow2
+       upload_image "${plugin}" "${username}" ${storm_1_0_1_image}
+       scenario_conf_file="$sahara_templates_path/storm-1.0.1.yaml.mako"
+       template_image_prefix="storm_1_0_1"
+    ;;
 esac
 
 cd $SAHARA_PATH
