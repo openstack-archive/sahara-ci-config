9033bb05a874e438e248192bd56b6c02a1cde0b0
Alina Nesterova <zemuvier@gmail.com>
2016-07-07 16:58:00 +0300

    Added storm 1.0.1 plugin
    Change-Id: I4bf9f5940754e714810e2d93b1b6ae573a518c9f

--- slave-scripts/functions-dib.sh
+++ slave-scripts/functions-dib.sh
@@ -80,15 +80,17 @@ upload_image() {
            spark_1.3.1)
              image_properties="--property _sahara_tag_spark=True --property _sahara_tag_1.3.1=True --property _sahara_username=${username}"
            ;;
-	   spark_1.6.0)
+	       spark_1.6.0)
              image_properties="--property _sahara_tag_spark=True --property _sahara_tag_1.6.0=True --property _sahara_username=${username}"
            ;;
            mapr_5.0.0.mrv2)
              image_properties="--property _sahara_tag_mapr=True --property _sahara_tag_5.0.0.mrv2=True --property _sahara_username=${username}"
            ;;
-	   mapr_5.1.0.mrv2)
+           mapr_5.1.0.mrv2)
              image_properties="--property _sahara_tag_mapr=True --property _sahara_tag_5.1.0.mrv2=True --property _sahara_username=${username}"
            ;;
+           storm_1.0.1)
+             image_properties="--property _sahara_tag_storm=True --property _sahara_tag_1.0.1=True --property _sahara_username=${username}"
    esac
    register_new_image "$image" "$image_properties"
    CUR_IMAGE="$image"
