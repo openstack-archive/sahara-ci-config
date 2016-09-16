8e1ee40d41a6d2e3cfdb102ddca962e083505a72
Evgeny Sikachev <esikachev@mirantis.com>
2016-09-16 16:27:25 +0300

    Added check for compatibility jjb jobs and layout
    We need to check if there no extra jobs in jjb
    
    Change-Id: Ide20b781ced77d6b0c101dac9a78cb457dbb776d

--- host-scripts/check_layout.py
+++ host-scripts/check_layout.py
@@ -8,6 +8,14 @@ import yaml
 
 layout = yaml.load(open(sys.argv[1]))
 
+JENKINS_JOBS = [
+    'update-pool',
+    'daily-log-publisher-43',
+    'integration-cleanup',
+    'jobs-updater',
+    'daily-log-publisher-42',
+    'update-config'
+]
 
 def grep(source, pattern):
     found = False
@@ -40,14 +48,25 @@ def check_jobs():
     with open(job_list_file, 'r') as f:
         job_list = [line.rstrip() for line in f]
 
+    zuul_jobs = []
+
     for project in layout['projects']:
         for pipeline in pipelines:
             jobs = project.get(pipeline, [])
             for job in jobs:
+                zuul_jobs.append(job)
                 found = grep(job_list, job)
                 if not found:
                     print ("Regex %s has no matches in job list" % job)
                     errors = True
+
+    for job in JENKINS_JOBS:
+        job_list.remove(job)
+
+    for job in job_list[1:]:
+        if job not in zuul_jobs:
+            print ("Job %s has no matches in zuul layout" % job)
+            errors = True
 
     return errors
 
