43b0c732c0adbc6065c1590e1ce127167e5d4991
Evgeny Sikachev <esikachev@mirantis.com>
2016-09-16 16:27:25 +0300

    Added check for compatibility jjb jobs and layout
    We need to check if there no extra jobs in jjb
    
    Change-Id: Ide20b781ced77d6b0c101dac9a78cb457dbb776d

--- host-scripts/check_layout.sh
+++ host-scripts/check_layout.sh
@@ -7,3 +7,4 @@ jenkins-jobs test -o out/ $WORKSPACE/jenkins_job_builder/
 find out/ -printf "%f\n" > job-list.txt
 
 python $WORKSPACE/host-scripts/check_layout.py $WORKSPACE/config/zuul/layout.yaml job-list.txt
+rm -rf out job-list.txt
